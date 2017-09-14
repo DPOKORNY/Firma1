c302ae8b6e76eaec2cb7b941bae5dd989a1eb70f
Tom Fajtl <ttt@gmail.com>
2017-09-14 08:47:38 +0200

    1.commit

---
+++ Firma1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Firma1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ONas()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+
+        public ActionResult WebPages()
+        {
+            return View();
+        }
+
+        public ActionResult WebApp()
+        {
+            return View();
+        }
+    }
+}
