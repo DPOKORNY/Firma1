a84f1d21d66e29ee4c40c0be006b0c88990fd1a2
Tom Fajtl <ttt@gmail.com>
2017-09-20 08:38:16 +0200

    2.commit

---
+++ Firma1/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Firma1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Firma1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            Heslo heslo = new Heslo();
+            return View(heslo);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Heslo heslo)
+        {
+
+            
+             if(heslo.ZvalidujHeslo())   
+            return RedirectToAction("Index","Home");
+            return View(heslo);
+        }
+    }
+}
---
+++ Firma1/Models/Heslo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Firma1.Models
+{
+    public class Heslo
+    {
+        private string heslo = "Aa47Loi369";
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public bool ZvalidujHeslo()
+        {
+            if (Password == heslo)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    
+}
